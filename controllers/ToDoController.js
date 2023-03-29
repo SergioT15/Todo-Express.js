d427b76224872f435987f9dfb3a38789f2f8ee6e
SergioT15 <sergiotitov15@gmail.com>
2023-03-29 15:34:17 +0300

    "Added update, deleted, completed"

--- controllers/ToDoController.js
+++ controllers/ToDoController.js
@@ -14,3 +14,24 @@ module.exports.saveToDo = async (req, res) => {
     res.send(data);
   });
 };
+
+module.exports.updateToDo = async (req, res) => {
+  const { _id, text } = req.body;
+  ToDoModel.findByIdAndUpdate(_id, { text })
+    .then(() => res.send("Updated Successfully"))
+    .catch((err) => console.log(err));
+};
+
+module.exports.deleteToDo = async (req, res) => {
+  const { _id } = req.body;
+  ToDoModel.findByIdAndDelete(_id)
+    .then(() => res.send("Deleted Successfully"))
+    .catch((err) => console.log(err));
+};
+
+module.exports.completedToDo = async (req, res) => {
+  const { _id, completed } = req.body;
+  ToDoModel.findByIdAndUpdate(_id, { completed })
+    .then(() => res.send("Completed Successfully"))
+    .catch((err) => console.log(err));
+};
--- routes/ToDoRoute.js
+++ routes/ToDoRoute.js
@@ -1,9 +1,18 @@
 const { Router } = require("express");
-const { getToDo, saveToDo } = require("../controllers/ToDoController");
+const {
+  getToDo,
+  saveToDo,
+  updateToDo,
+  deleteToDo,
+  completedToDo,
+} = require("../controllers/ToDoController");
 
 const router = Router();
 
 router.get("/", getToDo);
 router.post("/save", saveToDo);
+router.put("/update", updateToDo);
+router.delete("/delete", deleteToDo);
+router.put("/completed", completedToDo);
 
 module.exports = router;
